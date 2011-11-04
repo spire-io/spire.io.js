be28d233208f1094386522023cae7b9135a86682
Jason Campbell <jason@greatergood.cc>
2011-11-04 11:54:07 -0700

    specs for $.spire.messages.publish

--- test/specs.js
+++ test/specs.js
@@ -104,26 +104,6 @@ describe('jquery.spire.js', function(){
             expect(message.key).toBeDefined();
           });
         });
-
-        //     newMessage = { content: 'hi' };
-        //
-        //
-        //     $.spire
-        //       .requests
-        //       .channels
-        //       .create('random channel too', function(err, channel){
-        //         $.spire
-        //           .requests
-        //           .messages
-        //           .create(channel, newMessage.content, callback);
-        //       });
-        //
-        //     waitsFor(function(){ return callback.called; }, '', 10000);
-        //
-        //   });
-        // });
-
-
       });
     }); // describe('subscribe', ...
 
@@ -132,18 +112,44 @@ describe('jquery.spire.js', function(){
         expect($.spire.messages.publish).toBeDefined();
       });
 
+      it('should handle a callback', function(){
+        var channel = 'rowboat cop ' + (new Date().getTime())
+          , callback = sinon.spy()
+          , OGmessage = { channel: channel
+            , content: 'Abed was the meanest mean girl'
+            }
+        ;
+
+        $.spire.messages.publish(OGmessage, callback);
+
+        waitsFor(function(){ return callback.called; }, '', 10000);
+
+        runs(function(){
+          expect(callback).toHaveBeenCalled();
+
+          var err = callback.getCall(0).args[0]
+            , message = callback.getCall(0).args[1]
+          ;
+
+          expect(err).toBeFalsy();
+
+          expect(message).toBeDefined();
+          expect(message.content).toBe(OGmessage.content);
+        });
+      });
+
+      it('should not require a callback', function(){
+        var channel = 'rowboat cop ' + (new Date().getTime())
+          , message = { channel: channel
+            , content: 'Abed was the meanest mean girl'
+            }
+        ;
+
+        expect(function(){
+          $.spire.messages.publish(message);
+        }).not.toThrow();
+      });
     }); // describe('subscribe', ...
-
-    /***********************************************************************
-
-    $.spire.messages.subscribe('some channel', function(err, messages){
-      // whatever
-    });
-
-    $.spire.messages.publish('channel name', message, callback);
-
-
-    ***********************************************************************/
   }); // describe('messages', ...
 
   xdescribe('requests', function(){

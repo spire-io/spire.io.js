1bf781d3f739aac9d96eb7e4fcf18988a902c76a
Jason Campbell <jason@greatergood.cc>
2011-12-06 15:13:00 -0800

    test account helper

--- test/jasmine/helpers.js
+++ test/jasmine/helpers.js
@@ -1,3 +1,40 @@
+var helpers = {}
+;
+
+//`helpers.createAccount(callback)` - async: makes an async call to the
+// spire.io API to create a test account and triggers the `callback`, the
+// `callback` will be triggered with two args `err`, and the authenticated
+// `session` for the newly created account:
+//
+//     var account
+//     ;
+//
+//     helpers.account(function(err, session){
+//       if (err) throw err;
+//       else account = session.resources.account;
+//     });
+//
+// `helpers.account()` also wraps any work needed to handle async in the
+// jasmine test suite so it can be used in a `beforeEach` call without the
+// need to worry about async setup. Your `it` blocks will simply wait for the
+// `helpers.account()` function to do it's work before running.
+helpers.account = function(callback){
+  var properties = { email: 'test-' + (new Date().getTime()) + '@spire.io'
+      , password: 'super-secret'
+      }
+    , done
+  ;
+
+  waitsFor(function(){ return done; }
+  , 'waiting for test account ' + properties.email
+  , 10000);
+
+  $.spire.accounts.create(properties, function(err, session){
+    done = true;
+
+    runs(function(){ callback(err, session); });
+  });
+};
 
 beforeEach(function(){
   this.addMatchers({

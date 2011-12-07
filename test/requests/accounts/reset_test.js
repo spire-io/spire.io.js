ac7edbccf10ea7bf9c8e3e3ca06f7043106ec9be
Jason Campbell <jason@greatergood.cc>
2011-12-07 15:56:47 -0800

    account reset tests

--- test/requests/accounts/reset_test.js
+++ test/requests/accounts/reset_test.js
@@ -1,46 +1,45 @@
-//       describe('reset', function(){
-//         it('$.spire.requests.accounts.reset should exist', function(){
-//           expect($.spire.requests.accounts.reset).toBeDefined();
-//         });
-//
-//         it('can get a success', function(){
-//           var callback = sinon.spy()
-//             , stamp = new(Date)().getTime()
-//             , account = { email: 'random-' + stamp + '@email.com'
-//               , password: 'totallysecure'
-//               }
-//             , key
-//           ;
-//
-//           // create the account first
-//           $.spire.requests.description.get(function(){
-//             $.spire.requests.accounts.create(account, function(err, session){
-//               var account = session.resources.account
-//               ;
-//
-//               key = account.key;
-//
-//               account.email = 'different-' + stamp + '@test.com';
-//               account.password = 'notsosecure';
-//
-//               $.spire.requests.accounts.reset(account, callback);
-//             });
-//           });
-//
-//           waitsFor(function(){ return callback.called; }, '', 10000);
-//
-//           runs(function(){
-//             expect(callback).toHaveBeenCalled();
-//
-//             var err = callback.getCall(0).args[0]
-//               , account = callback.getCall(0).args[1]
-//             ;
-//
-//             expect(err).toBeFalsy();
-//
-//             expect(account).toBeAResourceObject();
-//             expect(account).toHaveACapability();
-//             expect(account.key).not.toBe(key);
-//           });
-//         });
-//       }); // describe('reset', ...
+describe('$.spire.requests.accounts.reset', function(){
+  var account
+  ;
+
+  beforeEach(function(){
+    helpers.account(function(err, session){
+      if (err) throw err;
+      else account = session.resources.account;
+    });
+  });
+
+  it('should exist', function(){
+    expect($.spire.requests.accounts.reset).toBeDefined();
+  });
+
+  it('should create an account', function(){
+    var callback = sinon.spy()
+      , key = account.key
+    ;
+
+    $.spire.requests.accounts.reset(account, callback);
+
+    waitsFor(function(){ return callback.called; }
+    , 'waiting for account reset'
+    , 10000);
+
+    runs(function(){
+      var err = callback.getCall(0).args[0]
+        , account = callback.getCall(0).args[1]
+      ;
+
+      expect(err).toBeFalsy();
+
+      expect(account).toBeAResourceObject();
+      expect(account).toHaveACapability();
+      expect(account.key).not.toBe(key);
+    });
+  });
+
+  describe('when there are errors', function(){
+    it('should pass errors to the callback', function(){
+      this.fail('needs error handling tests and code');
+    });
+  }); // describe('when there are errors', ...
+});

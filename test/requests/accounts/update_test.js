39f1a345e9d217633161abb208b26a1beefbec8f
Jason Campbell <jason@greatergood.cc>
2011-12-07 15:43:06 -0800

    account update tests

--- test/requests/accounts/update_test.js
+++ test/requests/accounts/update_test.js
@@ -1,66 +1,48 @@
-//
-//       describe('update', function(){
-//         it('$.spire.requests.accounts.update should exist', function(){
-//           expect($.spire.requests.accounts.update).toBeDefined();
-//         });
-//
-//         it('can get a success', function(){
-//           var callback = sinon.spy()
-//             , sessionBack = sinon.spy()
-//             , stamp = new(Date)().getTime()
-//             , account = { email: 'random-' + stamp + '@email.com'
-//               , password: 'totallysecure'
-//               }
-//             , email
-//           ;
-//
-//           // create the account first
-//           $.spire.requests.description.get(function(){
-//             $.spire.requests.accounts.create(account, function(err, session){
-//               var account = session.resources.account
-//               ;
-//
-//               email = 'different-' + stamp + '@test.com';
-//
-//               account.email = email;
-//               account.password = 'notsosecure';
-//
-//               $.spire.requests.accounts.update(account, callback);
-//             });
-//           });
-//
-//           waitsFor(function(){ return callback.called; }, '', 10000);
-//
-//           runs(function(){
-//             expect(callback).toHaveBeenCalled();
-//
-//             var err = callback.getCall(0).args[0]
-//               , account = callback.getCall(0).args[1]
-//             ;
-//
-//             expect(err).toBeFalsy();
-//
-//             expect(account).toBeAResourceObject();
-//             expect(account).toHaveACapability();
-//             expect(account.email).toBe(email);
-//
-//             $.spire.accounts.authenticate({ email: account.email
-//             , password: account.password
-//             }, sessionBack);
-//           });
-//
-//           waitsFor(function(){ return sessionBack.called; }, '', 10000);
-//
-//           runs(function(){
-//             expect(sessionBack).toHaveBeenCalled();
-//
-//             var err = callback.getCall(0).args[0]
-//               , account = callback.getCall(0).args[1]
-//             ;
-//
-//             expect(err).toBeFalsy();
-//
-//             console.log('session after password change');
-//           });
-//         });
-//       }); // describe('update', ...
+describe('$.spire.requests.accounts.update', function(){
+  var account
+  ;
+
+  beforeEach(function(){
+    helpers.account(function(err, session){
+      if (err) throw err;
+      else account = session.resources.account;
+    });
+  });
+
+  it('should exist', function(){
+    expect($.spire.requests.accounts.update).toBeDefined();
+  });
+
+  it('should update an account', function(){
+    var callback = sinon.spy()
+      , email = helpers.randomEmail()
+    ;
+
+    account.email = email;
+    account.password = 'blahblah';
+
+    $.spire.requests.accounts.update(account, callback);
+
+    waitsFor(function(){ return callback.called; }
+    , 'waiting for account creation'
+    , 10000);
+
+    runs(function(){
+      var err = callback.getCall(0).args[0]
+        , account = callback.getCall(0).args[1]
+      ;
+
+      expect(err).toBeFalsy();
+
+      expect(account).toBeAResourceObject();
+      expect(account).toHaveACapability();
+      expect(account.email).toBe(email);
+    });
+  });
+
+  describe('when there are errors', function(){
+    it('should pass errors to the callback', function(){
+      this.fail('needs error handling tests and code');
+    });
+  }); // describe('when there are errors', ...
+});

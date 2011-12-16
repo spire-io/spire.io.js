c28fe1a04022aa6cb3ff10d30853288e5a2d33e3
Jason Campbell <jason@greatergood.cc>
2011-12-16 12:29:35 -0800

    description err tests

--- test/requests/description/get_test.js
+++ test/requests/description/get_test.js
@@ -36,8 +36,38 @@ describe('$.spire.requests.description.get', function(){
   });
 
   describe('when there are errors', function(){
+    var stub
+    ;
+
+    beforeEach(function(){
+      $.spire.resources = null;
+
+      stub = sinon.stub(jQuery, 'ajax', function(options){
+        return options.error();
+      });
+    });
+
+    afterEach(function(){
+      jQuery.ajax.restore();
+    });
+
     it('should pass errors to the callback', function(){
-      this.fail('needs error handling tests and code');
+      var callback = sinon.spy()
+      ;
+
+      $.spire.requests.description.get(callback);
+
+      waitsFor(function(){ return callback.called; }
+      , 'waiting on the description request'
+      , 10000);
+
+
+      runs(function(){
+        var err = callback.getCall(0).args[0]
+        ;
+
+        expect(err).toBeTruthy();
+      });
     });
   }); // describe('when there are errors', ...
 }); // describe('$.spire.requests.description.get', ...

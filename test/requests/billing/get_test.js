b9428e20fbd90c9f2901859909ceb454c368c5a3
Jason Campbell <jason@greatergood.cc>
2011-12-16 12:24:54 -0800

    billing err tests

--- test/requests/billing/get_test.js
+++ test/requests/billing/get_test.js
@@ -45,8 +45,48 @@ describe('$.spire.requests.billing.get', function(){
   });
 
   describe('when there are errors', function(){
+    var stub
+      , account
+    ;
+
+    beforeEach(function(){
+      helpers.account(function(err, session){
+        if (err) throw err;
+        else account = session.resources.account;
+
+        $.spire.options.key = account.key;
+
+        stub = sinon.stub(jQuery, 'ajax', function(options){
+          return options.error();
+        });
+      });
+    });
+
+    afterEach(function(){
+      jQuery.ajax.restore();
+    });
+
     it('should pass errors to the callback', function(){
-      this.fail('needs error handling tests and code');
+      var callback = sinon.spy()
+      ;
+
+      $.spire.requests.description.get(function(err, desc){
+        if (err) throw err;
+
+        $.spire.requests.billing.get(callback);
+      });
+
+      waitsFor(function(){ return callback.called; }
+      , 'waiting for stuff'
+      , 10000);
+
+
+      runs(function(){
+        var err = callback.getCall(0).args[0]
+        ;
+
+        expect(err).toBeTruthy();
+      });
     });
   }); // describe('when there are errors', ...
 });

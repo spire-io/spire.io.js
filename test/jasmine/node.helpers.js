69e2931e9c72e20bd2ea3b337e23717d39ce6318
Matthew King <mking@borderstylo.com>
2012-01-31 12:25:21 -0800

    use SPIRE_URL envvar for test:node

--- test/jasmine/node.helpers.js
+++ test/jasmine/node.helpers.js
@@ -1,3 +1,7 @@
 if (typeof module === 'object' && module.exports) {
   module.exports.spire = require('../../spire.io.js');
+  var spire_url;
+  if (spire_url = process.env["SPIRE_URL"]) {
+    module.exports.spire.options.url = spire_url;
+  }
 }

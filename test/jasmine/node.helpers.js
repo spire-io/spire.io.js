fbe3b7b2e76933c79cd0879d5dae42a3f01acd08
Nicolas LaCasse <nlacasse@borderstylo.com>
2012-02-01 12:23:37 -0800

    pass in the options when constructing Spire

--- test/jasmine/node.helpers.js
+++ test/jasmine/node.helpers.js
@@ -1,8 +1,9 @@
 if (typeof module === 'object' && module.exports) {
   var Spire = require('../../lib/spire.io.js');
-  module.exports.spire = new Spire();
-  var spire_url;
-  if (spire_url = process.env["SPIRE_URL"]) {
-    module.exports.spire.options.url = spire_url;
+  var opts = {};
+  if (process.env["SPIRE_URL"]) {
+    opts.url = process.env["SPIRE_URL"];
   }
+
+  module.exports.spire = new Spire(opts);
 }

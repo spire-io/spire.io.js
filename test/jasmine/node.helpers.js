448c90a29e374d5d1002ed6a5b51d0bc8b2a4422
Nicolas LaCasse <nlacasse@borderstylo.com>
2012-01-31 17:28:48 -0800

    change the location of spire.io.js in the node tests

--- test/jasmine/node.helpers.js
+++ test/jasmine/node.helpers.js
@@ -1,5 +1,5 @@
 if (typeof module === 'object' && module.exports) {
-  module.exports.spire = require('../../spire.io.js');
+  module.exports.spire = require('../../lib/spire.io.js');
   var spire_url;
   if (spire_url = process.env["SPIRE_URL"]) {
     module.exports.spire.options.url = spire_url;

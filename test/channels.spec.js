c86ca056b37b03711f348dcdbcfc142ea7c7d927
Nicolas LaCasse <nlacasse@borderstylo.com>
2012-02-10 14:59:51 -0800

    more tests

---
+++ test/channels.spec.js
@@ -0,0 +1,52 @@
+var noop = function () {};
+
+describe('Channels', function () {
+  beforeEach(function () {
+    var finished = false;
+
+    runs(function () {
+      this.spire = createSpire();
+
+      if (this.key) {
+        this.spire.start(this.key, function (err) {
+          finished = true;
+        });
+      } else {
+        this.spire.register({
+          email: helpers.randomEmail(),
+          password: 'foobarbaz'
+        }, function (err) {
+          finished = true;
+        });
+      }
+    });
+
+    waitsFor(function () {
+      return finished;
+    }, 'Session registration or start', 10000);
+  });
+
+  describe('Create a channel', function () {
+    beforeEach(function () {
+      var finished = false;
+
+      runs(function () {
+        var that = this;
+        this.spire.channel('foo', function (err, channel) {
+          finished = true;
+          that.channel = channel;
+        });
+      });
+
+      waitsFor(function () {
+        return finished;
+      }, 'channel get or create', 10000);
+    });
+
+    it('should return a channel resource', function () {
+      expect(this.channel).toBeTruthy();
+      expect(this.channel).toBeAResourceObject();
+    });
+  }); // Create a channel
+
+}); // Channels

e549ec442d72ebd8a9ea9fb48803467aeee740f8
Nicolas LaCasse <nlacasse@borderstylo.com>
2012-02-10 13:43:51 -0800

    more test passing

--- test/spire.io.spec.js
+++ test/spire.io.spec.js
@@ -59,10 +59,7 @@ describe('Accounts and Session', function () {
 
           var finished = false;
           runs(function () {
-            this.spire.login({
-              email: this.email,
-              password: 'foobarbaz'
-            }, function (err) {
+            this.spire.login(this.email, 'foobarbaz', function (err) {
               finished = true;
             });
           });
@@ -72,17 +69,71 @@ describe('Accounts and Session', function () {
           }, 'spire.register', 10000);
         });
 
-      }) // Log in the with given email and password
+        it('should have a session', function () {
+          expect(this.spire.session).toBeTruthy();
+          expect(this.spire.session).toBeAResourceObject();
+        });
 
-      it('should have a session', function () {
-        expect(this.spire.session).toBeTruthy();
-        expect(this.spire.session).toBeAResourceObject();
-      });
+        it('should have a session with account resource', function () {
+          expect(this.spire.session.resources.account).toBeTruthy();
+          expect(this.spire.session.resources.account).toBeAResourceObject();
+        });
 
-      it('should have a session with account resource', function () {
-        expect(this.spire.session.resources.account).toBeTruthy();
-        expect(this.spire.session.resources.account).toBeAResourceObject();
-      });
+        describe('Change your password', function () {
+          var err;
+          beforeEach(function () {
+            var finished = false;
+            runs(function () {
+              this.spire.update({
+                email: this.email,
+                password: 'a new password'
+              }, function (e) {
+                err = e;
+                finished = true;
+              });
+            });
+
+            waitsFor(function () {
+              return finished;
+            }, 'spire.register', 10000);
+          });
+
+          it('should not throw an error', function () {
+            expect(err).toBeFalsy();
+          });
+
+          describe('Log in with the new password', function () {
+            beforeEach(function () {
+              this.spire = createSpire();
+
+              var finished = false;
+              runs(function () {
+                this.spire.login(this.email, 'a new password', function (err) {
+                  finished = true;
+                });
+              });
+
+              waitsFor(function () {
+                return finished;
+              }, 'spire.register', 10000);
+            });
+
+            it('should have a session', function () {
+              expect(this.spire.session).toBeTruthy();
+              expect(this.spire.session).toBeAResourceObject();
+            });
+
+            it('should have a session with account resource', function () {
+              expect(this.spire.session.resources.account).toBeTruthy();
+              expect(this.spire.session.resources.account).toBeAResourceObject();
+            });
+
+          }); // Log in with the new password
+
+            
+
+        }); // Change your password
+      }); // Log in the with given email and password
     }); // Registration with valid email and password
   }); // Registration and authentication
 }); // Accounts and sessions

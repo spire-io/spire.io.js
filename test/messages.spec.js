4e22f6d08d5f6757142d9e591f7a6aae91c4d68c
Nicolas LaCasse <nlacasse@borderstylo.com>
2012-04-17 12:19:20 -0700

    add tests for message resources

--- test/messages.spec.js
+++ test/messages.spec.js
@@ -22,5 +22,107 @@ describe('Messages', function(){
       return finished;
     }, 'Session registration or start', 10000);
   });
+
+  describe('A message returned from a publish request', function () {
+    beforeEach(function () {
+      var finished = false;
+
+      runs(function () {
+        var that = this;
+        this.spire.publish('a channel', 'my message', function (err, message) {
+          that.message = message;
+          finished = true;
+        });
+      });
+
+      waitsFor(function () {
+        return finished;
+      }, 10000, 'publish');
+    });
+
+    it('should have capabilities for get, update, and delete', function () {
+      expect(this.message.capabilities().get).toBeTruthy();
+      expect(this.message.capabilities().update).toBeTruthy();
+      expect(this.message.capabilities()['delete']).toBeTruthy();
+    });
+
+    describe('getting the message', function () {
+      beforeEach(function () {
+        var finished = false;
+
+        runs(function () {
+          var that = this;
+          this.message.get(function (err, newMessage) {
+            that.newMessage = newMessage;
+            finished = true;
+          });
+        });
+
+        waitsFor(function () {
+          return finished;
+        }, 10000, 'get messages');
+      });
+
+      it('should have the same url', function () {
+        expect(this.newMessage.url()).toBe(this.message.url());
+      });
+
+      it('should have the same content', function () {
+        expect(this.newMessage.content).toBe('my message');
+      });
+    });
+  });
+
+  describe('A message returned from a subscription', function () {
+    beforeEach(function () {
+      var finished = false;
+
+      runs(function () {
+        var that = this;
+        this.spire.publish('message channel', 'my message', function (err, message) {
+          that.spire.subscribe('message channel', function (messages) {
+            that.message = messages[0];
+            finished = true;
+          });
+        });
+      });
+
+      waitsFor(function () {
+        return finished;
+      }, 10000, 'get subscription');
+    });
+
+    it('should have capabilities for get, update, and delete', function () {
+      expect(this.message.capabilities().get).toBeTruthy();
+      expect(this.message.capabilities().update).toBeTruthy();
+      expect(this.message.capabilities()['delete']).toBeTruthy();
+    });
+
+    describe('getting the message', function () {
+      beforeEach(function () {
+        var finished = false;
+
+        runs(function () {
+          var that = this;
+          this.message.get(function (err, newMessage) {
+            that.newMessage = newMessage;
+            finished = true;
+          });
+        });
+
+        waitsFor(function () {
+          return finished;
+        }, 10000, 'get messages');
+      });
+
+      it('should have the same url', function () {
+        expect(this.newMessage.url()).toBe(this.message.url());
+      });
+
+      it('should have the same content', function () {
+        expect(this.newMessage.content).toBe('my message');
+      });
+    });
+  });
 });
 
